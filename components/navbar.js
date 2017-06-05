fd74e8ec82f00d40bab9c182f5d130d398bfd042
Robin <rtwilborn@gmail.com>
2017-06-05 11:06:50 -0700

    i hate this

--- components/navbar.js
+++ components/navbar.js
@@ -4,15 +4,15 @@ import { View, Text, StyleSheet } from 'react-native';
 class Navbar extends React.Component {
   render() {
     return (
-      <View id="Navbar" style={styles.Navbar}></View>
+      <View id="Navbar" style={styles.navbar}></View>
     );
   }
 }
 
 export default Navbar;
 const styles = StyleSheet.create({
-  Navbar: {
+  navbar: {
     height: 20,
-    backgroundColor: "blue"
+    backgroundColor: "dodgerblue"
   }
 });
