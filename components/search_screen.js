2bc214e28849455700f057b5f04cfb27114aad3e
Robin <rtwilborn@gmail.com>
2017-06-06 18:02:48 -0700

    Implement modals for search criteria

--- components/search_screen.js
+++ components/search_screen.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Navbar from './navbar';
-import { Text, View, StyleSheet, Button, Picker } from 'react-native';
+import { Text, View, StyleSheet, Button, TextInput } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import ModalPicker from 'react-native-modal-picker'
 
-const Item = Picker.Item;
 const PRICES = {
   "$": "1",
   "$$": "2",
   "$$$": "3",
   "$$$$": "4"
 };
+
+dataPrice = Object.keys(PRICES).map((price, idx) => (
+  {key: idx, label: price}
+));
 
 const CATEGORIES = {
   "American": "newamerican",
@@ -22,7 +26,7 @@ const CATEGORIES = {
   "Delis": "delis",
   "Diners": "diners",
   "Fast Food": "hotdogs",
-  "Hawaiian": "hawaiin",
+  "Hawaiian": "hawaiian",
   "Indian": "indpak",
   "Italian": "italian",
   "Japanese": "japanese",
@@ -41,6 +45,10 @@ const CATEGORIES = {
   "Waffles": "waffles"
 }
 
+dataCategory = Object.keys(CATEGORIES).map((cat, idx) => (
+  {key: idx, label: cat}
+));
+
 const DISTANCE = {
   "1 Mile": 1609.34,
   "2 Miles": 3218.69,
@@ -54,14 +62,17 @@ const DISTANCE = {
   "10 Miles": 16093.4,
 };
 
+dataDistance = Object.keys(DISTANCE).map((dist, idx) => (
+  {key: idx, label: dist}
+));
+
 class SearchScreen extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      category: 'key1',
-      price: 'key2',
-      distance: 'key3',
-      mode: Picker.MODE_DIALOG
+      category: '',
+      price: '',
+      distance: ''
     };
   }
   static navigationOptions = {
@@ -69,75 +80,106 @@ class SearchScreen extends React.Component{
   };
 
 
-
-  onValueChange = (key: string, value: string) => {
-    const newState = {};
-    newState[key] = value;
-    this.setState(newState);
-  }
-
   render(){
     const { navigate } = this.props.navigation;
     return (
       <View style={styles.search}>
         <Text style={styles.welcome}>What are you feeling?</Text>
-        <View>
-          <Text>Price</Text>
-          <Picker
-            style={styles.picker}
-            selectedValue={this.state.price}
-            onValueChange={this.onValueChange.bind(this, 'price')}>
-            {Object.keys(PRICES).map((price, idx) => (
-              <Item key={idx} label={price} value={PRICES[price]} />
-            ))}
-          </Picker>
+        <View style={styles.container}>
+          <Text style={styles.label}>Price</Text>
+          <ModalPicker
+            data={dataPrice}
+            style={{flex: 1}}
+            initValue="Select Price Range"
+            onChange={(option)=>{ this.setState({price:option.label})}}>
+            <Text style={styles.defaultText}>
+              {this.state.price === '' ? "Select Price" : this.state.price}
+            </Text>
+          </ModalPicker>
         </View>
 
-        <View>
-          <Text>Category</Text>
-          <Picker
-            style={styles.picker}
-            selectedValue={this.state.category}
-            onValueChange={this.onValueChange.bind(this, 'category')}>
-            {Object.keys(CATEGORIES).map((category, idx) => (
-              <Item key={idx} label={category} value={CATEGORIES[category]} />
-            ))}
-          </Picker>
+        <View style={styles.container}>
+          <Text style={styles.label}>Category</Text>
+          <ModalPicker
+            data={dataCategory}
+            style={{flex: 1}}
+            initValue="Select Category"
+            onChange={(option)=>{ this.setState({category:option.label})}}>
+            <Text style={styles.defaultText}>
+              {this.state.category === '' ? "Select Category" : this.state.category}
+            </Text>
+          </ModalPicker>
         </View>
 
-        <View>
-          <Text>Distance</Text>
-          <Picker
-            style={styles.picker}
-            selectedValue={this.state.distance}
-            onValueChange={this.onValueChange.bind(this, 'distance')}>
-            {Object.keys(DISTANCE).map((distance, idx) => (
-              <Item key={idx} label={distance} value={DISTANCE[distance]} />
-            ))}
-          </Picker>
+        <View style={styles.container}>
+          <Text style={styles.label}>Distance</Text>
+          <ModalPicker
+            data={dataDistance}
+            style={{flex: 1}}
+            initValue="Select Distance"
+            onChange={(option)=>{ this.setState({distance:option.label})}}>
+            <Text style={styles.defaultText}>
+              {this.state.distance === '' ? "Select Distance" : this.state.distance}
+            </Text>
+          </ModalPicker>
         </View>
+
+
 
       </View>
     );
   }
 }
 
-// Object.keys(PRICES).forEach((price) => (
-//   <Item label="hello" value="key1" />)
-// )
 export default SearchScreen;
 
 const styles = StyleSheet.create({
   search: {
     alignItems: "center",
-    justifyContent: "space-around"
+    justifyContent: "center",
+    height: 500
   },
 
   welcome: {
-    fontSize: 30
+    fontSize: 30,
+    marginBottom: 30
   },
 
-  picker: {
-    width: 100,
+  defaultText: {
+    fontSize: 24,
+  },
+
+  container: {
+    alignItems: 'center',
+    flex: 1
+  },
+
+  label: {
+    fontSize: 30,
+    fontWeight: 'bold'
   }
 })
+
+// <View>
+//   <Text>Category</Text>
+//   <Picker
+//     style={styles.picker}
+//     selectedValue={this.state.category}
+//     onValueChange={this.onValueChange.bind(this, 'category')}>
+//     {Object.keys(CATEGORIES).map((category, idx) => (
+//       <Item key={idx} label={category} value={CATEGORIES[category]} />
+//     ))}
+//   </Picker>
+// </View>
+//
+// <View>
+//   <Text>Distance</Text>
+//   <Picker
+//     style={styles.picker}
+//     selectedValue={this.state.distance}
+//     onValueChange={this.onValueChange.bind(this, 'distance')}>
+//     {Object.keys(DISTANCE).map((distance, idx) => (
+//       <Item key={idx} label={distance} value={DISTANCE[distance]} />
+//     ))}
+//   </Picker>
+// </View>
