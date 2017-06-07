21e846ad052ede588bf1d432a34bd5863cf70917
Robin <rtwilborn@gmail.com>
2017-06-07 11:33:31 -0700

    Implement Yelp fusion API results

--- components/search_screen.js
+++ components/search_screen.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Navbar from './navbar';
-import { Text, View, StyleSheet, Button, TextInput } from 'react-native';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-import ModalPicker from 'react-native-modal-picker'
+import ModalPicker from 'react-native-modal-picker';
+import Button from 'react-native-button';
 
 const PRICES = {
   "$": "1",
-  "$$": "2",
-  "$$$": "3",
-  "$$$$": "4"
+  "$$": "1,2",
+  "$$$": "1,2,3",
+  "$$$$": "1,2,3,4"
 };
 
 dataPrice = Object.keys(PRICES).map((price, idx) => (
@@ -50,21 +51,22 @@ dataCategory = Object.keys(CATEGORIES).map((cat, idx) => (
 ));
 
 const DISTANCE = {
-  "1 Mile": 1609.34,
-  "2 Miles": 3218.69,
-  "3 Miles": 4828.03,
-  "4 Miles": 6437.38,
-  "5 Miles": 8046.72,
-  "6 Miles": 9656.06,
-  "7 Miles": 11265.4,
-  "8 Miles": 12874.8,
-  "9 Miles": 14484.1,
-  "10 Miles": 16093.4,
+  "1 Mile": 1609,
+  "2 Miles": 3218,
+  "3 Miles": 4828,
+  "4 Miles": 6437,
+  "5 Miles": 8046,
+  "6 Miles": 9656,
+  "7 Miles": 11265,
+  "8 Miles": 12874,
+  "9 Miles": 14484,
+  "10 Miles": 16093,
 };
 
 dataDistance = Object.keys(DISTANCE).map((dist, idx) => (
   {key: idx, label: dist}
 ));
+
 
 class SearchScreen extends React.Component{
   constructor(props){
@@ -72,16 +74,35 @@ class SearchScreen extends React.Component{
     this.state = {
       category: '',
       price: '',
-      distance: ''
+      distance: '',
+      latitude: 33.68,
+      longitude: -117.82
     };
+    this.handleButton = this.handleButton.bind(this)
   }
+
+  componentDidMount(){
+    const Navigator = window.navigator;
+    Navigator.geolocation.getCurrentPosition(
+      position => this.setState({latitude: position.coords.latitude, longitude: position.coords.longitude}),
+      () => true);
+  }
+
   static navigationOptions = {
     title: 'Search',
   };
 
+  handleButton(e){
+    e.preventDefault();
+    const { navigate } = this.props.navigation;
+    alert(this.state.longitude)
+    const url = `https://api.yelp.com/v3/businesses/search?term=restaurants&latitude=${this.state.latitude}&longitude=${this.state.longitude}&categories=${CATEGORIES[this.state.category]}&radius=${DISTANCE[this.state.distance]}&price=${PRICES[this.state.price]}`
+    navigate('Result', {url})
+  }
+
 
   render(){
-    const { navigate } = this.props.navigation;
+
     return (
       <View style={styles.search}>
         <Text style={styles.welcome}>What are you feeling?</Text>
@@ -91,7 +112,6 @@ class SearchScreen extends React.Component{
             data={dataPrice}
             style={{flex: 1}}
             initValue="Select Price Range"
-            keyboardShouldPersistTaps= "always"
             onChange={(option)=>{ this.setState({price:option.label})}}>
             <Text style={styles.defaultText}>
               {this.state.price === '' ? "Select Price" : this.state.price}
@@ -105,7 +125,6 @@ class SearchScreen extends React.Component{
             data={dataCategory}
             style={{flex: 1}}
             initValue="Select Category"
-            keyboardShouldPersistTaps= "always"
             onChange={(option)=>{ this.setState({category:option.label})}}>
             <Text style={styles.defaultText}>
               {this.state.category === '' ? "Select Category" : this.state.category}
@@ -119,7 +138,6 @@ class SearchScreen extends React.Component{
             data={dataDistance}
             style={{flex: 1}}
             initValue="Select Distance"
-            keyboardShouldPersistTaps= "always"
             onChange={(option)=>{ this.setState({distance:option.label})}}>
             <Text style={styles.defaultText}>
               {this.state.distance === '' ? "Select Distance" : this.state.distance}
@@ -127,8 +145,11 @@ class SearchScreen extends React.Component{
           </ModalPicker>
         </View>
 
+        <Button containerStyle={styles.button}
+          onPress={this.handleButton} >
+          <Text style={styles.buttonText}>Search</Text>
 
-
+        </Button>
       </View>
     );
   }
@@ -160,5 +181,13 @@ const styles = StyleSheet.create({
   label: {
     fontSize: 30,
     fontWeight: 'bold'
+  },
+
+  button: {
+
+  },
+
+  buttonText: {
+
   }
 })
