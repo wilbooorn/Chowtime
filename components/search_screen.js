5b4bb35d6480074782ab04609ce2afe555a313e7
Robin <rtwilborn@gmail.com>
2017-06-09 11:30:55 -0700

    Updates to search page

--- components/search_screen.js
+++ components/search_screen.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Navbar from './navbar';
-import { Text, View, StyleSheet, TextInput, Image } from 'react-native';
+import { Text, View, StyleSheet, TextInput, Image, Slider } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import ModalPicker from 'react-native-modal-picker';
-import Button from 'apsl-react-native-button'
+import Button from 'apsl-react-native-button';
+import { SegmentedControls} from 'react-native-radio-buttons';
 
 const PRICES = {
   "$": "1",
@@ -51,36 +52,28 @@ dataCategory = Object.keys(CATEGORIES).map((cat, idx) => (
 ));
 
 const DISTANCE = {
-  "1 Mile": 1609,
-  "2 Miles": 3218,
-  "3 Miles": 4828,
-  "4 Miles": 6437,
-  "5 Miles": 8046,
-  "6 Miles": 9656,
-  "7 Miles": 11265,
-  "8 Miles": 12874,
-  "9 Miles": 14484,
-  "10 Miles": 16093,
+  1: 1609,
+  2: 3218,
+  3: 4828,
+  4: 6437,
+  5: 8046,
+  6: 9656,
+  7: 11265,
+  8: 12874,
+  9: 14484,
+  10: 16093,
 };
-
-dataDistance = Object.keys(DISTANCE).map((dist, idx) => (
-  {key: idx, label: dist}
-));
 
 
 class SearchScreen extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      // category: '',
-      category: 'American',
-      // price: '',
+      category: '',
       price: '$',
-      // distance: '',
-      distance: '2 Miles',
+      distance: 5,
       latitude: 33.68,
       longitude: -117.82,
-      // disabled: true
       disabled: false
     };
     this.handleButton = this.handleButton.bind(this)
@@ -103,19 +96,24 @@ class SearchScreen extends React.Component{
   handleButton(e){
     e.preventDefault();
     const { navigate } = this.props.navigation;
-    const url = `https://api.yelp.com/v3/businesses/search?term=restaurants&latitude=${this.state.latitude}&longitude=${this.state.longitude}&categories=${CATEGORIES[this.state.category]}&radius=${DISTANCE[this.state.distance]}&price=${PRICES[this.state.price]}`
-    navigate('Result', {url})
+    if(this.state.category ===  ''){
+      const url = `https://api.yelp.com/v3/businesses/search?term=restaurants&latitude=${this.state.latitude}&longitude=${this.state.longitude}&radius=${DISTANCE[this.state.distance]}&price=${PRICES[this.state.price]}`
+      navigate('Result', {url})
+    } else {
+      const url = `https://api.yelp.com/v3/businesses/search?term=restaurants&latitude=${this.state.latitude}&longitude=${this.state.longitude}&categories=${CATEGORIES[this.state.category]}&radius=${DISTANCE[this.state.distance]}&price=${PRICES[this.state.price]}`
+      navigate('Result', {url})
+    }
   }
 
   handlePrice(option){
-    this.setState({price: option.label})
+    this.setState({price: option})
     if(this.state.distance !== '' && this.state.category !== ''){
       this.setState({disabled: false})
     }
   }
 
-  handleDistance(option){
-    this.setState({distance:option.label})
+  handleDistance(value){
+    this.setState({distance:value})
     if(this.state.price !== '' && this.state.category !== ''){
       this.setState({disabled: false})
     }
@@ -134,43 +132,50 @@ class SearchScreen extends React.Component{
     return (
       <View style={styles.search}>
         <Image source={require('../assets/white-text-logo.png')} style={styles.logo} />
-        <View style={styles.container}>
-          <Text style={styles.label}>Price</Text>
-          <ModalPicker
-            data={dataPrice}
-            style={{flex: 1}}
-            initValue="Select Price Range"
-            onChange={(option) => this.handlePrice(option)}>
-            <Text style={styles.defaultText}>
-              {this.state.price === '' ? "Select Price" : this.state.price}
-            </Text>
-          </ModalPicker>
-        </View>
 
-        <View style={styles.container}>
-          <Text style={styles.label}>Category</Text>
-          <ModalPicker
-            data={dataCategory}
-            style={{flex: 1}}
-            initValue="Select Category"
-            onChange={(option) => this.handleCategory(option)}>
-            <Text style={styles.defaultText}>
-              {this.state.category === '' ? "Select Category" : this.state.category}
-            </Text>
-          </ModalPicker>
-        </View>
+        <View style={styles.searchFields}>
+          <View style={styles.container}>
+            <Text style={styles.label}>Category</Text>
+            <ModalPicker
+              data={dataCategory}
+              style={{height: 60}}
+              initValue="Select Category"
+              optionStyle={{backgroundColor:'rgb(255,255,255)'}}
+              cancelStyle={{backgroundColor:'rgb(255,255,255)'}}
+              onChange={(option) => this.handleCategory(option)}>
+              <Text style={styles.defaultText}>
+                {this.state.category === '' ? "Select Category" : this.state.category}
+              </Text>
+            </ModalPicker>
+          </View>
 
-        <View style={styles.container}>
-          <Text style={styles.label}>Distance</Text>
-          <ModalPicker
-            data={dataDistance}
-            style={{flex: 1}}
-            initValue="Select Distance"
-            onChange={(option) => this.handleDistance(option)}>
-            <Text style={styles.defaultText}>
-              {this.state.distance === '' ? "Select Distance" : this.state.distance}
+          <View style={styles.containerPrice}>
+            <Text style={styles.label}>Price</Text>
+            <SegmentedControls
+              options={['$', '$$', '$$$', '$$$$']}
+              onSelection={this.handlePrice}
+              optionStyle={{height: 30, paddingTop: 5, fontSize: 18}}
+              selectedOption={this.state.price}>
+
+            </SegmentedControls>
+
+          </View>
+
+
+          <View>
+            <Text style={styles.label}>Distance</Text>
+            <Slider
+              value={5}
+              step={1}
+              minimumValue={1}
+              maximumValue={10}
+              onValueChange={value => this.handleDistance(value)}>
+            </Slider>
+            <Text style={styles.sliderText}>
+              { `${this.state.distance} ` + (this.state.distance === 1 ? 'Mile' : 'Miles') }
             </Text>
-          </ModalPicker>
+          </View>
+
         </View>
 
         <Button style={styles.button}
@@ -192,34 +197,51 @@ const styles = StyleSheet.create({
   search: {
     flex: 1,
     alignItems: "center",
-    backgroundColor: 'dodgerblue'
+    backgroundColor: "#27343D"
+  },
+
+  searchFields: {
+    flex: 1,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    bottom: 40
   },
 
   welcome: {
     fontSize: 24,
     marginTop: 5,
-    marginBottom: 10
+    marginBottom: 5
   },
 
   defaultText: {
     fontSize: 24,
+    fontWeight: 'bold',
     backgroundColor: 'white',
     padding: 8,
     borderColor: 'black',
     borderWidth: 1,
+    borderRadius: 4,
+    overflow: 'hidden',
     width: 250,
     textAlign: 'center'
   },
 
   container: {
     alignItems: 'center',
-    flex: 1
+    marginBottom: 20
+  },
+
+  containerPrice: {
+    alignItems: 'center',
+    marginBottom: 30
   },
 
   label: {
     fontSize: 30,
     fontWeight: 'bold',
-    marginBottom: 5
+    marginBottom: 8,
+    color: 'white',
+    alignSelf: 'center'
   },
 
   button: {
@@ -241,5 +263,12 @@ const styles = StyleSheet.create({
   logo: {
     marginTop: 20,
     marginBottom: 10
+  },
+
+  sliderText: {
+    color: 'white',
+    fontSize: 18,
+    alignSelf: 'center',
+    marginTop: 8
   }
 })
